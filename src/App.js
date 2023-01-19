9831d4c8681c3afc73c9bd9975de71360fac4a4c
Marvyn Kiplagat <marvynrogony@gmail.com>
2023-01-19 10:53:08 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
+const App = () => {
+    return (
+      <div className="app">
+
+      </div>
+    )
+  }
+
+export default App;
